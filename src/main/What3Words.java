4fa6ab9a4b48808f4174d6d3417991587b3a606d
Baburin Sergey <sbasba777@hotmail.com>
2015-05-16 19:53:23 +0300

    3 words to position

--- src/main/What3Words.java
+++ src/main/What3Words.java
@@ -1,18 +1,24 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class What3Words {
 
+    private static final String URL = "http://api.what3words.com";
     private static final String API_KEY_FILE = "api.key";
+
+    private final String apiKey;
+
+    private What3Words() {
+        apiKey = getApiKey(API_KEY_FILE);
+    }
 
     public static void main(String... args) {
 
         What3Words w3w = new What3Words();
-        System.out.println(w3w.getApiKey(API_KEY_FILE));
+        System.out.println(w3w.get3Words(args[0]));
     }
 
     private String getApiKey(String file) {
@@ -26,5 +32,41 @@ public class What3Words {
             e.printStackTrace();
         }
         return apiKey;
+    }
+
+    private String getURL () {
+        return String.format("%s/w3w?key=%s&string=",
+                URL,
+                apiKey);
+    }
+
+    private String get3Words(String string) {
+
+        StringBuilder result = new StringBuilder();
+        URL url;
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        try {
+            url = new URL(getURL() + string);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            String input;
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while ((input = reader.readLine()) != null) {
+                result.append(input);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (connection != null) connection.disconnect();
+        }
+
+        return result.toString();
     }
 }
