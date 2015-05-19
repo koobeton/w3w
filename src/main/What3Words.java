4df123d838488140e0d49bffe92ce7e5dfeda13e
Baburin Sergey <sbasba777@hotmail.com>
2015-05-19 21:54:09 +0300

    Add JSON parsing

--- src/main/What3Words.java
+++ src/main/What3Words.java
@@ -1,8 +1,16 @@
 package main;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
 import static main.W3WApiMethod.*;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -12,9 +20,11 @@ public class What3Words {
     private static final String W3W_URL = "http://api.what3words.com";
     private static final String API_KEY_FILE = "api.key";
 
-    private static String threeWords;
-    private static BigDecimal latitude;
-    private static BigDecimal longitude;
+    private String threeWords = null;
+    private BigDecimal latitude = null;
+    private BigDecimal longitude = null;
+    private String error = null;
+    private String errorMessage = null;
 
     private final String apiKey;
 
@@ -24,26 +34,31 @@ public class What3Words {
 
     public static void main(String... args) {
 
-        handleArgs(args);
-
         What3Words w3w = new What3Words();
-        if (threeWords != null) {
-            System.out.println(w3w.getPosition(threeWords));
-        } else if (latitude != null && longitude != null) {
-            System.out.println(w3w.get3Words(latitude, longitude));
+
+        handleArgs(w3w, args);
+
+        if (w3w.threeWords != null) {
+            w3w.setPosition(w3w.threeWords);
+        } else if (w3w.latitude != null && w3w.longitude != null) {
+            w3w.set3Words(w3w.latitude, w3w.longitude);
+        }
+
+        w3w.showResults();
+    }
+
+    private void setPosition(String threeWords) {
+        if (is3Words(threeWords)) {
+            parseJson(getJson(buildURLString(W3W, threeWords)));
         } else {
+            System.out.printf("Not a 3 words: %s%n", threeWords);
             showUsage();
+            System.exit(0);
         }
     }
 
-    private String getPosition(String threeWords) {
-        return is3Words(threeWords) ?
-                getJson(buildURLString(W3W, threeWords)) :
-                String.format("Not a 3 words: %s", threeWords);
-    }
-
-    private String get3Words(BigDecimal latitude, BigDecimal longitude) {
-        return getJson(buildURLString(POSITION, String.format("%s,%s", latitude, longitude)));
+    private void set3Words(BigDecimal latitude, BigDecimal longitude) {
+        parseJson(getJson(buildURLString(POSITION, String.format("%s,%s", latitude, longitude))));
     }
 
     private boolean is3Words(String words) {
@@ -72,45 +87,73 @@ public class What3Words {
                 query);
     }
 
-    private String getJson(String urlString) {
+    private JsonObject getJson(String urlString) {
 
-        StringBuilder result = new StringBuilder();
         URL url;
         HttpURLConnection connection = null;
-        BufferedReader reader = null;
+        JsonReader reader = null;
+        JsonObject result = null;
 
         try {
             url = new URL(urlString);
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
-            String input;
-            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            while ((input = reader.readLine()) != null) {
-                result.append(input);
-            }
+            reader = Json.createReader(connection.getInputStream());
+            result = reader.readObject();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            try {
-                if (reader != null) reader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            if (reader != null) reader.close();
             if (connection != null) connection.disconnect();
         }
 
-        return result.toString();
+        return result;
     }
 
-    private static void handleArgs(String... args) {
+    private void parseJson(JsonObject object) {
+
+        if (object == null) return;
+
+        JsonArray words = object.getJsonArray("words");
+        if (words != null) {
+            threeWords = String.format("%s.%s.%s",
+                    words.getString(0, null),
+                    words.getString(1, null),
+                    words.getString(2, null));
+        }
+
+        JsonArray position = object.getJsonArray("position");
+        if (position != null) {
+            latitude = position.getJsonNumber(0).bigDecimalValue();
+            longitude = position.getJsonNumber(1).bigDecimalValue();
+        }
+
+        error = object.getString("error", null);
+        errorMessage = object.getString("message", null);
+    }
+
+    private void showResults() {
+        if (error != null) {
+            System.out.printf("Error:\t\t%s%nMessage:\t%s%n",
+                    error,
+                    errorMessage);
+        } else {
+            System.out.printf("3 words:\t%s%nLatitude:\t%s%nLongitude:\t%s%n",
+                    threeWords,
+                    latitude,
+                    longitude);
+        }
+    }
+
+    private static void handleArgs(What3Words w3w, String... args) {
         switch (args.length) {
             case 3:
-                threeWords = String.format("%s.%s.%s", args[0], args[1], args[2]);
+                w3w.threeWords = String.format("%s.%s.%s", args[0], args[1], args[2]);
                 break;
             case 2:
                 try {
-                    latitude = new BigDecimal(args[0].replace(',', '.'));
-                    longitude = new BigDecimal(args[1].replace(',', '.'));
+                    w3w.latitude = new BigDecimal(args[0].replace(',', '.'));
+                    w3w.longitude = new BigDecimal(args[1].replace(',', '.'));
                 } catch (NumberFormatException e) {
                     System.out.println("Wrong number format");
                     showUsage();
